744678a4f9c54052f10a73ed9705562ce9df7798
Abraham Martinez <martinez.abraham@gmail.com>
2022-06-16 15:43:31 -0700

    init push

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -34,7 +34,7 @@ Follow-up: Could you solve the problem in linear time and in O(1) space?
 import java.util.HashMap;
 
 public class MajorityElement {
-    //T:O(N), S:O(1)
+    //T:O(N), S:O(1) Info
     //Boyer-Moore Voting Algorithm used to find the majority element among the given elements that have more than N/ 2 occurrences.
     //This algorithm works on the fact that if an element occurs more than N/2 times, it means that the remaining elements other than this would definitely be less than N/2. So let us check the proceeding of the algorithm.
     //First, choose a candidate from the given set of elements if it is the same as the candidate element, increase the votes. Otherwise, decrease the votes if votes become 0, select another new element as the new candidate
